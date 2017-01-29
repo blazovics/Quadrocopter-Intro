ee6003d65bb9caab7498cca1777567740a145a69
marton bognar <martonbognar@sch.bme.hu>
2017-01-29 23:28:52 +0100

    Remove unnecessary whitespaces

--- main.c
+++ main.c
@@ -15,15 +15,15 @@ enum powers{
 };
 
 int main(int argc, const char * argv[]) {
-    
+
     enum powers atomic = Japan;
     int i = 0;
     double x = Power(), y;
     double *px = &y;
     *px = x;
-    
+
     power(px);
-    
+
     if(*px == (x*atomic) && atomic == x)
     {
         for(i = 0; i<powerLevel(); i++)
@@ -31,6 +31,6 @@ int main(int argc, const char * argv[]) {
             printf("%s\n","Siker" NOSPACE "es" SPACE "futtatas");
         }
     }
-    
+
     return 0;
 }
